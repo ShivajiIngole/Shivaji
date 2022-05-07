a952cabb492be4446be3afc9d396a16c6f0858fd
ShivajiIngole <shivaji.ingole@gmail.com>
2022-05-07 10:11:41 +0530

    gitcommit test

---
+++ JavaBasicProgram/src/day1/basics/Hashmap.java
@@ -0,0 +1,20 @@
+package day1.basics;
+
+import java.util.HashMap;
+
+public class Hashmap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashMap m1= new HashMap();
+		m1.put("shivaji", "INgole");
+		m1.put(null, "Karbhari");
+		m1.put(1,3);
+		m1.put(null, "shiva");
+		System.out.println (m1);
+		
+		
+	}
+
+}
